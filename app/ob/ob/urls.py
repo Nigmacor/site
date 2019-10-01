167a087fc0e3e177b8bba9ee2cab6fd458ac7d5e
Denis <laevei@mail.ru>
2019-10-01 11:19:29 +0300

    rerestart

--- app/ob/ob/urls.py
+++ app/ob/ob/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home/', include('home.urls')),
 ]
