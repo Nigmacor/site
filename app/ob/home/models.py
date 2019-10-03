240dbc2bf1713871e664682f6e5d8aa01a458b41
aleksei <aleksei102rus@gmail.com>
2019-10-03 14:15:46 +0500

    register

--- app/ob/home/models.py
+++ app/ob/home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import timedelta
 
 
 # Create your models here.
@@ -8,7 +8,7 @@ class Orders(models.Model):
 	costomer = models.URLField(max_length=150, db_index=True) #ссылка на заказчика
 	body = models.TextField(blank=False, db_index=True)
 	date_pud = models.DateTimeField(auto_now_add=True)
-	duration = models.DurationField()#длительность тендора
+	duration = models.DurationField(default=timedelta(days=7))#длительность тендора
 		
 	def __str__(self):
 		return '{}'.format(self.title)
