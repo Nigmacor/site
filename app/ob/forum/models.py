31e6ccaa936477521184d8b1f12de4b5dddc93b8
aleksei102rus <aleksei102rus@gmail.com>
2019-10-06 11:05:59 +0500

    форум

--- app/ob/forum/models.py
+++ app/ob/forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=150, db_index=True)
+	user = models.URLField(max_length=150, db_index=True) 
+	body = models.TextField(blank=False, db_index=True)
+	date_pud = models.DateTimeField(auto_now_add=True)
+	views = models.IntegerField(default=0)
+	massenge = models.IntegerField(default=0)
+
+	def __str__(self):
+		return '{}'.format(self.title)
--- app/ob/forum/views.py
+++ app/ob/forum/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 
 def forum(request):
-	
-	return render(request, 'forum/forum.html')
+	posts = Post.objects.all()
+	return render(request, 'forum/forum.html', context={'posts': posts})
 
 def forum_detail(request):
 	
