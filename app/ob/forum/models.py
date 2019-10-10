8e2dc40fab35ebdfbad8b8d666e30ef20f8c5f69
Denis <laevei@mail.ru>
2019-10-10 11:24:37 +0300

    bugfix

--- app/ob/forum/models.py
+++ app/ob/forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 # Create your models here.
@@ -12,3 +13,7 @@ class Post(models.Model):
 
 	def __str__(self):
 		return '{}'.format(self.title)
+
+	def get_absolute_url(self):
+		return reverse('forum_detail_url', kwargs={'id': self.id})
+
