11493c0816520e5ac4557293a1d2e54ed45cc0bc
Nigmacor <laevei@mail.ru>
2019-11-12 16:10:56 +0500

    прока керня

--- app/ob/account/models.py
+++ app/ob/account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True,
+                                       verbose_name='Прошел активацию?')
+    send_messages = models.BooleanField(default=True,
+                    verbose_name='Слать оповещения о новых комментариях?')
+
+
+
+
+"""
+username - логин
+password - пароль в закодированном виде. Обязательно к заполнению;
+email - почта
+first _ name - имя
+last _ name - фамилия
+is_active - True, если онлайн
+is_staff - True, если персонал
+is_superuser - True, если админ
+last_login - дата и время последнего входа на сайт
+is_authenticated - возвращает True, если пользователь выполнил вход
+is_anonymous - как is_authenticated, но наоборот
+
+Если что, к ним нужно привязать шаблон
+"""
